9aca215b8a213e90377760a44482d466df14932a
Emil <ehakobyan96@gmail.com>
2017-06-27 01:56:28 +0400

    Second Commit :two:

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ToDo from './main';
-import registerServiceWorker from './registerServiceWorker';
-import './style.css';
-
-ReactDOM.render(<ToDo />, document.getElementById('root'));
-registerServiceWorker();
